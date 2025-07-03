c04b0fcdaa00f96047f77d92ecf44869395f737f
sama salloum <sma.sallom@hotmail.com>
2025-07-03 16:10:02 +0300

    add 360 view - cont

--- Farm_DT/Assets/FarmCrops/scripts/CameraSystem.cs
+++ Farm_DT/Assets/FarmCrops/scripts/CameraSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
@@ -11,6 +13,12 @@ public class CameraSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
     }
 }
