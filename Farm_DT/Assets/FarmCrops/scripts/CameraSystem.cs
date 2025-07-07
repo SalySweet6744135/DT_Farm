2fc747f3605266a89cb165c4f51511acbae700c7
sama salloum <sma.sallom@hotmail.com>
2025-07-07 08:43:47 +0300

    view cotroler mod

--- Farm_DT/Assets/FarmCrops/scripts/CameraSystem.cs
+++ Farm_DT/Assets/FarmCrops/scripts/CameraSystem.cs
@@ -1,24 +1,168 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Cinemachine;
 
-public class CameraSystem : MonoBehaviour
+namespace CodeMonkey.CameraSystem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CameraSystem : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+        [SerializeField] private Transform followTarget;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 moveDir = new Vector3(0, 0, 0);
+        [SerializeField] private bool useEdgeScrolling = false;
+        [SerializeField] private bool useDragPan = false;
+        [SerializeField] private float fieldOfViewMax = 50f;
+        [SerializeField] private float fieldOfViewMin = 10f;
+        [SerializeField] private float followOffsetMin = 5f;
+        [SerializeField] private float followOffsetMax = 50f;
+        [SerializeField] private float followOffsetMinY = 10f;
+        [SerializeField] private float followOffsetMaxY = 50f;
 
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        private bool dragPanMoveActive;
+        private Vector2 lastMousePosition;
+        private float targetFieldOfView = 50f;
+        private Vector3 followOffset;
 
+        private void Awake()
+        {
+            followOffset = followTarget.localPosition;
+        }
+
+        private void Update()
+        {
+            HandleCameraMovement();
+
+            if (useEdgeScrolling)
+            {
+                HandleCameraMovementEdgeScrolling();
+            }
+
+            if (useDragPan)
+            {
+                HandleCameraMovementDragPan();
+            }
+
+            HandleCameraRotation();
+
+            //HandleCameraZoom_FieldOfView();
+            //HandleCameraZoom_MoveForward();
+            HandleCameraZoom_LowerY();
+        }
+
+        private void HandleCameraMovement()
+        {
+            Vector3 inputDir = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
+            if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
+            if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
+            if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
+
+            Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+            float moveSpeed = 50f;
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        private void HandleCameraMovementEdgeScrolling()
+        {
+            Vector3 inputDir = Vector3.zero;
+            int edgeScrollSize = 20;
+
+            if (Input.mousePosition.x < edgeScrollSize) inputDir.x = -1f;
+            if (Input.mousePosition.y < edgeScrollSize) inputDir.z = -1f;
+            if (Input.mousePosition.x > Screen.width - edgeScrollSize) inputDir.x = +1f;
+            if (Input.mousePosition.y > Screen.height - edgeScrollSize) inputDir.z = +1f;
+
+            Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+            float moveSpeed = 50f;
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        private void HandleCameraMovementDragPan()
+        {
+            Vector3 inputDir = Vector3.zero;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                dragPanMoveActive = true;
+                lastMousePosition = Input.mousePosition;
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                dragPanMoveActive = false;
+            }
+
+            if (dragPanMoveActive)
+            {
+                Vector2 mouseMovementDelta = (Vector2)Input.mousePosition - lastMousePosition;
+
+                float dragPanSpeed = 1f;
+                inputDir.x = mouseMovementDelta.x * dragPanSpeed;
+                inputDir.z = mouseMovementDelta.y * dragPanSpeed;
+
+                lastMousePosition = Input.mousePosition;
+            }
+
+            Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+            float moveSpeed = 50f;
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        private void HandleCameraRotation()
+        {
+            float rotateDir = 0f;
+            if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
+            if (Input.GetKey(KeyCode.E)) rotateDir = -1f;
+
+            float rotateSpeed = 100f;
+            transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
+        }
+
+        private void HandleCameraZoom_FieldOfView()
+        {
+            if (Input.mouseScrollDelta.y > 0) targetFieldOfView -= 5f;
+            if (Input.mouseScrollDelta.y < 0) targetFieldOfView += 5f;
+
+            targetFieldOfView = Mathf.Clamp(targetFieldOfView, fieldOfViewMin, fieldOfViewMax);
+
+            float zoomSpeed = 10f;
+            cinemachineCamera.Lens.FieldOfView =
+                Mathf.Lerp(cinemachineCamera.Lens.FieldOfView, targetFieldOfView, Time.deltaTime * zoomSpeed);
+        }
+
+        private void HandleCameraZoom_MoveForward()
+        {
+            Vector3 zoomDir = followOffset.normalized;
+
+            float zoomAmount = 3f;
+            if (Input.mouseScrollDelta.y > 0) followOffset -= zoomDir * zoomAmount;
+            if (Input.mouseScrollDelta.y < 0) followOffset += zoomDir * zoomAmount;
+
+            followOffset = Vector3.ClampMagnitude(followOffset, followOffsetMax);
+            if (followOffset.magnitude < followOffsetMin)
+            {
+                followOffset = zoomDir * followOffsetMin;
+            }
+
+            float zoomSpeed = 10f;
+            followTarget.localPosition = Vector3.Lerp(followTarget.localPosition, followOffset, Time.deltaTime * zoomSpeed);
+        }
+
+        private void HandleCameraZoom_LowerY()
+        {
+            float zoomAmount = 3f;
+
+            if (Input.mouseScrollDelta.y > 0) followOffset.y -= zoomAmount;
+            if (Input.mouseScrollDelta.y < 0) followOffset.y += zoomAmount;
+
+            followOffset.y = Mathf.Clamp(followOffset.y, followOffsetMinY, followOffsetMaxY);
+
+            float zoomSpeed = 10f;
+            followTarget.localPosition = Vector3.Lerp(followTarget.localPosition, followOffset, Time.deltaTime * zoomSpeed);
+        }
     }
 }
